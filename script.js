591d81bcae93dba0edc6acab83113fdc05ee9f46
Elisa Capololo <elisacapololo2017@gmail.com>
2023-06-16 10:30:05 +0000

    add js document file

---
+++ script.js
@@ -0,0 +1,33 @@
+const addBox = document.querySelector('.add-box'),
+    popupBox = document.querySelector('.popup-box'),
+    popupTitle = document.querySelector('header p'),
+    closeIcon = document.querySelector('header i'),
+    titleNote = document.querySelector('input'),
+    descNote = document.querySelector('textarea'),
+    addBtn = document.querySelector('button');
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const notes = JSON.parse(localStorage.getItem('notes') || '[]');
+let isUpdate = false, updateId;
+
+function showNotes() {
+    document.querySelectorAll('.note').forEach(note => note.remove());
+    notes.forEach((note, index) => {
+        let liNote = `<li class="note">
+                        <div class="details">
+                            <p>${note.title}</p>
+                            <span>${note.description}</span>
+                        </div>
+                        <div class="bottom-content">
+                            <span>${note.date}</span>
+                            <div class="settings">
+                                <i onClick="updateNote(${index}, '${note.title}', '${note.description}')" class="uil uil-edit"></i>
+                                <i onClick="deleteNote(${index})" class="uil uil-trash"></i>
+                            </div>
+                        </div>
+                      </li>`;
+        addBox.insertAdjacentHTML('afterend', liNote);
+    });
+}
+
